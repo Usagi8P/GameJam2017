fb0bbb2fb6c3079f524cd89ff677d0160bf187f2
PiotrusWatson <piotruswatson@gmail.com>
2017-03-29 23:59:47 +0100

    added basic floor

--- Assets/Scripts/Inverter.cs
+++ Assets/Scripts/Inverter.cs
@@ -5,17 +5,17 @@ public class Inverter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-			AudioSource aud = GetComponent<AudioSource>();
+		AudioSource aud = GetComponent<AudioSource>();
+		bool allFloatsAreZero = true;
 		float[] samples = new float[aud.clip.samples * aud.clip.channels];
 		aud.clip.GetData(samples, 0);
 		int i = 0;
 		while (i < samples.Length) {
-			Debug.Log (samples [i]);
-			samples[i] = samples[i] * -1F;
-			Debug.Log (samples [i]);
+			samples[i] = samples[i] * 0.5F;
 			++i;
 		}
 		aud.clip.SetData(samples, 0);
+		Debug.Log (allFloatsAreZero);
 	}
 	
 	// Update is called once per frame

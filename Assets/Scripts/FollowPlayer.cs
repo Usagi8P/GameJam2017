9e867bb51710774e24e784c32ae6c5f7d36fdfda
PiotrusWatson <piotruswatson@gmail.com>
2017-03-30 03:29:58 +0100

    did a bit of work on playercontroller

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour {
-
+	public GameObject player;
+	Vector3 offset;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		offset = transform.position - player.transform.position;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = offset + player.transform.position;
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody2D rb2D;
+	Animator animator;
+	float moveX;
+
+	//publicly editable stuff :)
+	[SerializeField]
+	float velocity = 5.0f;
+	[SerializeField]
+	float jumpHeight = 5.0f;
+	[SerializeField]
+	string xaxis = "horizontal_K1";
+	[SerializeField]
+	string jump = "jump_K1";
+	[SerializeField]
+	string walkString = "colguywalk";
+	[SerializeField]
+	string idleString = "colguyidle";
+
+	//OPTIMIZATION :-)
+	int walkHash;
+	int idleHash;
+
+
+
+	//Animation stuff
+	bool isWalk;
+
+	// Use this for initialization
+	void Awake () {
+		rb2D = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+
+		walkHash = Animator.StringToHash ("colguywalk");
+		idleHash = Animator.StringToHash ("colguyidle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+	void FixedUpdate(){
+		moveX = Input.GetAxis (xaxis);
+		animator.SetFloat ("Speed", Mathf.Abs (moveX));
+	}
+}

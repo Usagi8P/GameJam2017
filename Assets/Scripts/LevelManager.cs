6b19a9b2a8b3d3cd7e4c3cad48bd24e2aa10b3d9
framacia <frankmmacia@gmail.com>
2017-03-31 02:18:44 +0000

    Intro and menu finished

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,7 +8,7 @@ public class LevelManager : MonoBehaviour {
     public void LoadLevel(string name)
     {
         Debug.Log("Level load requested for: " + name);
-        UnityEngine.SceneManagement.SceneManager.LoadScene("hell");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(name);
     }
 
     public void QuitGame()

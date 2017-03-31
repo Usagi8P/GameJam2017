c71db13765f553f9b3dc4167f067981b2d41ee05
PiotrusWatson <piotruswatson@gmail.com>
2017-03-31 14:20:30 +0100

    background prefab is a thing

---
+++ Assets/followcamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followcamera : MonoBehaviour {
+	GameObject MainCamera;
+	// Use this for initialization
+	void Awake () {
+		MainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = MainCamera.transform.position + new Vector3(0,0,10);
+	}
+}

55914ff0d133e2959ab1aa1ffa95dcd21af1e8ba
PiotrusWatson <piotruswatson@gmail.com>
2017-03-31 04:50:55 +0100

    fixed backflips

--- Assets/DoorLogic.cs
+++ Assets/DoorLogic.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorLogic : MonoBehaviour {
+	GameObject player;
 
+	[SerializeField]
+	string nextLevel;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		if (GameObject.FindGameObjectWithTag ("Player") != null) {
+			player = GameObject.FindGameObjectWithTag ("Player");
+		}
 	}
 	
 	// Update is called once per frame
